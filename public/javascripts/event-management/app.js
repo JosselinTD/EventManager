9456740b95cbe2d66c6b9d3478316ed6f8c3382a
JosselinTD <josselin.td@gmail.com>
2015-11-26 16:32:04 +0100

    [AngularJS] Creating the App and adding the gulp-generated js file to the scripts

---
+++ public/javascripts/event-management/app.js
@@ -0,0 +1,14 @@
+(function(){
+
+	/*
+		The event-management module handle all the operation on events :
+		- Get from the server
+		- Display
+		- Search / filter
+		- Add / Modify events
+
+		The events folder contain the logic for the event list and the event subfolder contain all the logic for each item of the list
+	*/
+
+	angular.module("event-management", []);
+})();
