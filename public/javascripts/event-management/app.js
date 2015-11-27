a2613d6165f9f598b4ea57b1af379e5b7cf94001
JosselinTD <josselin.td@gmail.com>
2015-11-27 11:41:05 +0100

    [Front] Change classic js bootstrap for angular bootstrap

--- public/javascripts/event-management/app.js
+++ public/javascripts/event-management/app.js
@@ -10,5 +10,5 @@
 		The events folder contain the logic for the event list and the event subfolder contain all the logic for each item of the list
 	*/
 
-	angular.module("event-management", ["ngResource"]);
+	angular.module("event-management", ["ngResource", "ui.bootstrap"]);
 })();
