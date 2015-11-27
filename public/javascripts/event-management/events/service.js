fc37b7baaca1ef50d301c01abcc07bd25147ccba
JosselinTD <josselin.td@gmail.com>
2015-11-27 11:44:59 +0100

    [AngularJS] Server communication

--- public/javascripts/event-management/events/service.js
+++ public/javascripts/event-management/events/service.js
@@ -2,12 +2,26 @@
 	angular.module("event-management")
 		.service("EventsService", ["$resource", function($resource){
 			var Events = $resource("/events/:id", null, {
-				update: {
-					method: "PUT"
-				}
-			}),
+						update: {
+							method: "PUT"
+						}
+					}
+				),
 				serv = this;
 
-			this.events = Events.query();
+			serv.events = Events.query();
+
+			serv.add = function(event, success, error){
+				success = success || function(){};
+				error = error || function(){};
+
+				var newEvent = new Events(event);
+				newEvent.$save({}, function(data){
+					serv.events.push(new Events(data));
+					success();
+				}, function(){
+					error();
+				});
+			}
 		}]);
 })();
