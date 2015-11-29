f80ab0758d54318dffc249de818150f047b5486a
JosselinTD <josselin.td@gmail.com>
2015-11-29 11:18:37 +0100

    [AngularJS] Add search

--- public/javascripts/event-management/events/controller.js
+++ public/javascripts/event-management/events/controller.js
@@ -2,5 +2,10 @@
 	angular.module("event-management")
 		.controller("EventsController", ["$scope", "EventsService", function($scope, EventsService){
 			$scope.events = EventsService.events;
+
+			$scope.searchFilter = function(obj){
+				var re = new RegExp($scope.search, 'i');
+				return !$scope.search || re.test(obj.title) || re.test(obj.description) || re.test(obj.date);
+			}
 		}]);
 })();
