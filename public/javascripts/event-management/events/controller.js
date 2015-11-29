7964989d25aa21c597698a3dced05fb20970e9d1
JosselinTD <josselin.td@gmail.com>
2015-11-29 12:29:50 +0100

    [AngularJS] Use the "past" attribute to display past events or not

--- public/javascripts/event-management/events/controller.js
+++ public/javascripts/event-management/events/controller.js
@@ -2,9 +2,13 @@
 	angular.module("event-management")
 		.controller("EventsController", ["$scope", "EventsService", function($scope, EventsService){
 			$scope.events = EventsService.events;
+			$scope.pastEvents = false;
 
 			$scope.searchFilter = function(obj){
 				var re = new RegExp($scope.search, 'i');
+				if(!$scope.pastEvents && obj.past){
+					return false;
+				}
 				return !$scope.search || re.test(obj.title) || re.test(obj.description) || re.test(obj.date);
 			}
 		}]);
